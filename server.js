498926d6e15b71c2b372e6e34cbdac27a0806a4b
sara khaled <sara.hrb1@hotmail.com>
2019-11-28 14:27:42 +0300

    for heroku

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const path = require("path");
+const mongoose = require("mongoose");
+const cors = require('cors')
+const methodOverride = require('method-override') 
+
+require('dotenv/config')
+
+const PORT = process.env.PORT
+
+
+
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "build")));
+app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'))
+app.set('view engine', 'ejs')
+app.use('/api/trips' , require('./routes/trip'))
+app.use('/api/trip' , require('./routes/tripJson'))
+app.use('/api/corses' , require('./routes/corses'))
+app.use('/api/corsess' , require('./routes/corsesJson'))
+app.use('/api/Profile' , require('./routes/Profile'))
+app.use('/api/user' , require('./routes/user'))
+//dev test
+
+mongoose.connect(
+   process.env.DB_CONNECTION,
+   { useNewUrlParser: true, useUnifiedTopology: true },
+   () => {
+     console.log(`connect tho mongoDB`);
+   }
+ );
+ //test dev
+ app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+ //i did this
+ app.listen(PORT);
